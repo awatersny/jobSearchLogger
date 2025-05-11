51dc4921dcec88d070d17bc97d615b783c47b161
awatersny <awatersny@gmail.com>
2025-05-10 23:46:59 -0400

    Add get requests for jobs and companies

---
+++ src/services/jobSearchLoggerAPI.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:5050"
+
+export async function getAllJobs() {
+  const URL = `${baseURL}/jobs`
+  const response = await axios.get(URL)
+  return response
+}
+
+export async function getJob(id) {
+  const URL = `${baseURL}/jobs/${id}`
+  const response = await axios.get(URL)
+  return response
+}
+export async function getAllCompanies() {
+  const URL = `${baseURL}/companies`
+  const response = await axios.get(URL)
+  return response
+}
+export async function getCompany(id) {
+  const URL = `${baseURL}/companies/${id}`
+  const response = await axios.get(URL)
+  return response
+}
