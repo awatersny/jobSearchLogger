fa702716c4b60ed58fe78f8a6ffbdb982feee35b
awatersny <awatersny@gmail.com>
2025-05-11 17:21:20 -0400

    Add starter components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Companies from './pages/Companies'
+import Jobs from './pages/Jobs'
+import NavBar from './components/NavBar'
 import './App.css'
+import Job from './pages/Job'
+import Company from './pages/Company'
+import EditJob from './pages/EditJob'
+import EditCompany from './pages/EditCompany'
+import CreateJob from './pages/CreateJob'
 
 function App() {
 
   return (
     <>
-      <h1>Job Search Tracker</h1>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/companies" element={<Companies/>}/>
+        <Route path="/companies/:id" element={<Company/>}/>
+        <Route path="/companies/:id/edit" element={<EditCompany/>}/>
+        <Route path="/jobs" element={<Jobs/>}/>
+        <Route path="/jobs/new" element={<CreateJob/>}/>
+        <Route path="/jobs/:id" element={<Job/>}/>
+        <Route path="/jobs/:id/edit" element={<EditJob/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+    <ul id='nav'>
+      <li><h1>Job Search Logger</h1></li>
+      <li><Link className='link' to="/jobs">Jobs List</Link></li>
+      <li><Link className='link' to="/companies">Companies List</Link></li>
+      <li><Link className='link' to="/jobs/new">Add Job</Link></li>
+    </ul>
+  )
+}
+
