44275270b11ea7f26da1567be50a0cb577d7cbd4
awatersny <awatersny@gmail.com>
2025-05-11 23:20:21 -0400

    Render company data

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllCompanies } from '../services/jobSearchLoggerAPI'
+import Company from './Company'
 
 export default function Companies() {
   const [companies, setCompanies] = useState([])
 
+  useEffect(() => {
+    getAllCompanies().then(res => setCompanies(res.data))
+  })
+
   return (
-    <div>Companies</div>
+    <>
+      <div>Companies</div>
+      {companies.map(company => <div>
+        <Company
+          name = {company.name}
+          description = {company.description}
+          values = {company.values}
+          website = {company.website}
+        />
+        <br />
+      </div>)}
+    </>
   )
 }
--- src/pages/Company.tsx
+++ src/pages/Company.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export default function Company() {
+export default function Company({ name, description, values, website }) {
   return (
-    <div>Company</div>
+    <>
+      <div><b>Company: </b>{name}</div>
+      <div><b>Description: </b>{description}</div>
+      <div><u><b>Values</b></u></div>
+      {values.length ? values.map(value => <div>
+        {value}
+      </div>) : <div>None Recorded</div>}
+      <div><b>Website: </b>{website}</div>
+    </>
   )
 }
