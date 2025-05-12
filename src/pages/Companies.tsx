267dcd783187ae9079d34dda19a073b911bcb76d
awatersny <awatersny@gmail.com>
2025-05-11 23:05:32 -0400

    Render jobs in database

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Companies() {
+  const [companies, setCompanies] = useState([])
+
   return (
     <div>Companies</div>
   )
--- src/pages/Job.tsx
+++ src/pages/Job.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export default function Job({ title }) {
+export default function Job({ title, description, skills, status }) {
   return (
-    <div>{title}</div>
+    <>
+      <div><b>Title: </b>{title}</div>
+      <div><b>Description: </b>{description}</div>
+      <div>
+      <u><b>Skills</b></u>
+        {skills.map(skill => <div>{skill}</div>)}
+      </div>
+      <div><b>Status: </b>{status}</div>
+      <br />
+    </>
   )
 }
