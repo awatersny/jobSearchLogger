4b0f6ea2264506711cf8f28a8900bf0402439b12
awatersny <awatersny@gmail.com>
2025-05-13 14:01:55 -0400

    Continue using baseURL for production build

--- src/services/jobSearchLoggerAPI.ts
+++ src/services/jobSearchLoggerAPI.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = "https://jobsearchloggerapi.onrender.com"
+// const baseURL = "https://jobsearchloggerapi.onrender.com"
+const baseURL = "http://localhost:5050"
+
 
 export async function getAllJobs() {
   const URL = `${baseURL}/jobs`
