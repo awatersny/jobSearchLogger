da9bbae6260c90adaa5b3d3e1cf9a8be62164c49
awatersny <awatersny@gmail.com>
2025-05-15 20:13:39 -0400

    Interact with deployed api

--- src/services/jobSearchLoggerAPI.ts
+++ src/services/jobSearchLoggerAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "https://jobsearchloggerapi.onrender.com"
-const baseURL = "http://localhost:5050"
+const baseURL = "https://jobsearchloggerapi.onrender.com"
+// const baseURL = "http://localhost:5050"
 
 
 export async function getAllJobs() {
